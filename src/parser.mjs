1b8163459bb23f3a1648a0947f7a1a50cff36c67
Arnau Mora <arnyminer.z@gmail.com>
2024-01-18 17:53:16 +0100

    Added `/poliformat` and `/intranet` endpoints
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- src/parser.mjs
+++ src/parser.mjs
@@ -8,3 +8,17 @@ export const fix = (original) => original
     .replaceAll('UPV_BGCOLOR', 'COLOR')
     // Remove fields with UPV_FGCOLOR
     .replaceAll(/UPV_FGCOLOR:.*\r?\n|\r/g, '');
+
+/**
+ * Converts the given content from HTML to Markdown.
+ * Supported features:
+ * - Bold (`<b></b>`)
+ * - Line breaks (`<br/>`)
+ * @param {string} original
+ * @returns {string}
+ */
+export const convertMarkdown = (original) => original
+    // Replace bold HTML annotations to Markdown
+    .replace(/(<b>)|(<\/b>)/gm, '**')
+    // Replace HTML line-break annotations to \n
+    .replace(/<br\/?>/gm, '\\n');
